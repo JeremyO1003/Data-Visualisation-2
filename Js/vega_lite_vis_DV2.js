cec4d3abf56b1316590b0ff64ca1fdd3294d5d55
JeremyO1003 <jobr0025@student.monash.edu>
2026-05-20 05:18:36 +1000

    Add files via upload

---
+++ Js/vega_lite_vis_DV2.js
@@ -0,0 +1,8 @@
+var vg_2 = "Js/premierships_bar.vg.json";
+
+var embedOpt = {"actions": false};
+
+
+
+vegaEmbed("#premierships_bar", vg_2, embedOpt).then(function(result) {
+}).catch(console.error);
