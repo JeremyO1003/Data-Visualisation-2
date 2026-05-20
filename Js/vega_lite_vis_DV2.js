5df53e88a5d7712807d19536e3f310d2d625ff4c
JeremyO1003 <jobr0025@student.monash.edu>
2026-05-20 13:23:16 +1000

    added third graph vega_lite_vis_DV2.js

--- Js/vega_lite_vis_DV2.js
+++ Js/vega_lite_vis_DV2.js
@@ -1,8 +1,12 @@
 var vg_2 = "Js/premierships_bar.vg.json";
+var vg_3 = "Js/attendance_line.vg.json";
 
 var embedOpt = {"actions": false};
 
 
 
 vegaEmbed("#premierships_bar", vg_2, embedOpt).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#attendance_line", vg_3, embedOpt).then(function(result) {
 }).catch(console.error);
