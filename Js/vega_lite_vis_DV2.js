815ca0fca40a4c3ea432892cef203aa8785111c1
JeremyO1003 <jobr0025@student.monash.edu>
2026-06-02 23:39:44 +1000

    Update vega_lite_vis_DV2.js

--- Js/vega_lite_vis_DV2.js
+++ Js/vega_lite_vis_DV2.js
@@ -1,10 +1,18 @@
+var vg_1 = "Js/venue_map.vg.json";
 var vg_2 = "Js/premierships_bar.vg.json";
 var vg_3 = "Js/attendance_line.vg.json";
 var vg_4 = "Js/scoring_trends.vg (1).json";
+var vg_5 = "Js/goalkickers_dot.vg.json";
+var vg_6 = "Js/home_away_dumbbell.vg.json";
+var vg_7 = "Js/ladder_2025.vg.json";
+var vg_8 = "Js/brownlow_chart.vg.json";
+var vg_9 = "Js/teams_by_state.vg.json";
+var vg_10 = "Js/venue_attendance_bar.vg.json";
 
 var embedOpt = {"actions": false};
 
-
+vegaEmbed("#venue_map", vg_1, embedOpt).then(function(result) {
+}).catch(console.error);
 
 vegaEmbed("#premierships_bar", vg_2, embedOpt).then(function(result) {
 }).catch(console.error);
@@ -13,4 +21,22 @@ vegaEmbed("#attendance_line", vg_3, embedOpt).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed("#scoring_trends", vg_4, embedOpt).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#goalkickers_dot", vg_5, embedOpt).then(function(result) {
+}).catch(console.error);
+ 
+vegaEmbed("#home_away_dumbbell", vg_6, embedOpt).then(function(result) {
+}).catch(console.error);
+ 
+vegaEmbed("#ladder_2025", vg_7, embedOpt).then(function(result) {
+}).catch(console.error);
+ 
+vegaEmbed("#brownlow_chart", vg_8, embedOpt).then(function(result) {
+}).catch(console.error);
+ 
+vegaEmbed("#teams_by_state", vg_9, embedOpt).then(function(result) {
+}).catch(console.error);
+ 
+vegaEmbed("#venue_attendance_bar", vg_10, embedOpt).then(function(result) {
 }).catch(console.error);
